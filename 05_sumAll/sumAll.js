d1913ecc101073ec79a2332d3ed25027d5ca0c91
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-07 13:25:43 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+   let min = Math.min(int1, int2);
+   let max = Math.max(int1, int2);
+   
+   let sum = 0; 
+   if (int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)) {
+    return "ERROR";
+   } else {
+    for (let i = min; i <= max; i++) {
+    sum += i;
+   }
+    }
+   return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
