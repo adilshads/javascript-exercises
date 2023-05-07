21281476ed02f2c45c33b3a50986df50d556ef03
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-07 14:40:40 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  let degreesCelsius = (degreesF - 32) * (5/9);
+  let roundedDegreesCelsius = degreesCelsius.toFixed(1);
+  let floatCelsius = parseFloat(roundedDegreesCelsius);
+  return floatCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  let degreesFahrenheit = (degreesC * (9/5)) + 32;
+  let roundedDegreesFahrenheit = degreesFahrenheit.toFixed(1);
+  let floatFahrenheit = parseFloat(roundedDegreesFahrenheit);
+  return floatFahrenheit;
 };
 
 // Do not edit below this line
