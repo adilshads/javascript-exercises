b43cb02153a0ea8bb2f0edddedbd2b296f3e7eaf
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-07 12:45:35 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...arg) {
+    for (let i = 0; i < array.length; i++) {
+        if (arg.includes(array[i])) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
