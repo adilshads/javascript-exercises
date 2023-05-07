28d4a2394e66da3a9ba6c44f29e3f21025dc8173
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-07 12:10:54 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
 };
 
+let input = 'hello';
+let output = reverseString(input);
 // Do not edit below this line
 module.exports = reverseString;
