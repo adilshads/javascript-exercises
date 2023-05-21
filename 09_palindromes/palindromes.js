60c7c9e1bf3d6ab0cd34792da3db8ea2f723ff7e
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-21 18:02:40 -0400

    Complete exercises and tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
+const palindromes = function(n) {
+    const string = n;
+    const array = Array.from(string.toLowerCase()).filter((char) => /[a-z]/.test(char));
+    const reversedArray = [...array].reverse();
+  
+    return array.join("") === reversedArray.join("");
+  };
+  
 
 // Do not edit below this line
 module.exports = palindromes;
