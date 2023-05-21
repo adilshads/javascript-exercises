03469e8ca0f3cf4eb152628b198dcebaeb5442fc
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-21 17:45:27 -0400

    Finish exercises and test them

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	c = a + b; 
+  return c;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	c = a - b; 
+  return c; 
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((total, current) => total + current);
 };
 
-const multiply = function() {
+
+const multiply = function(numbers) {
+  return numbers.reduce((a, b) => a * b);
+};
+
+
+const power = function(a, b) {
+  c = a ** b;
+  return c;
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
