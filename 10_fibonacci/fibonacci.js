edc11df721bfff7f1e45cd63b9bc5ac04d0d4867
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-21 18:26:47 -0400

    Complete exercises and tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    if (typeof n === 'string') {
+      n = Number(n);
+    }
+  
+    if (typeof n !== 'number' || n < 0) {
+      return "OOPS";
+    }
+  
+    let a = 0;
+    let b = 1;
+  
+    if (n === 0) {
+      return a;
+    }
+  
+    for (let i = 2; i <= n; i++) {
+      const nextNumber = a + b;
+      a = b;
+      b = nextNumber;
+    }
+  
+    return b;
+  };
+  
 
 // Do not edit below this line
 module.exports = fibonacci;
