0afa57c6e17c983817ff74e18da8661d577b4554
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-21 19:43:59 -0400

    Comeplete exercise and tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const today = new Date();
+    return people.reduce((oldestPerson, currentPerson) => {
+      const oldestAge = (oldestPerson.yearOfDeath || today.getFullYear()) - oldestPerson.yearOfBirth;
+      const currentAge = (currentPerson.yearOfDeath || today.getFullYear()) - currentPerson.yearOfBirth;
+      return currentAge > oldestAge ? currentPerson : oldestPerson;
+    });
+  };
+  
 
 // Do not edit below this line
 module.exports = findTheOldest;
